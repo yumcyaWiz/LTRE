6d811e656eeebe98436efc9125ce4d0b735dc7ce
yumcyawiz <hashdolf1923@gmail.com>
2021-05-18 02:42:16 +0900

    add Accel

---
+++ include/LTRE/accel/accel.hpp
@@ -0,0 +1,25 @@
+#ifndef _LTRE_ACCEL_H
+#define _LTRE_ACCEL_H
+#include <vector>
+
+#include "LTRE/core/primitive.hpp"
+
+namespace LTRE {
+
+class Accel {
+ private:
+  std::vector<Primitive> primitives;
+
+ public:
+  Accel() {}
+
+  void addPrimitive(const Primitive& primitive) {
+    primitives.push_back(primitive);
+  }
+
+  virtual bool intersect(const Ray& ray, IntersectInfo& info) const = 0;
+};
+
+}  // namespace LTRE
+
+#endif
