dd4a6d640cab2f501cf51f1e7c99cdddb614968a
yumcyawiz <hashdolf1923@gmail.com>
2021-05-17 17:27:56 +0900

    add vec2

---
+++ include/LTRE/math/vec2.hpp
@@ -0,0 +1,157 @@
+#ifndef _LTRE_VEC2_H
+#define _LTRE_VEC2_H
+#include <cassert>
+#include <cmath>
+
+namespace LTRE {
+
+struct Vec2 {
+  float v[2];
+
+  explicit constexpr Vec2() : v{0, 0} {}
+  explicit constexpr Vec2(float x) : v{x, x} {}
+  explicit constexpr Vec2(float x, float y) : v{x, y} {}
+
+  constexpr bool operator==(const Vec2& v) const {
+    return this->v[0] == v[0] && this->v[1] == v[1];
+  }
+
+  constexpr const float& operator[](int i) const {
+    assert(i >= 0 && i <= 1);
+    return v[i];
+  }
+  constexpr float& operator[](int i) {
+    assert(i >= 0 && i <= 1);
+    return v[i];
+  }
+
+  constexpr Vec2 operator-() const { return Vec2(-v[0], -v[1]); }
+
+  constexpr Vec2& operator+=(const Vec2& v) {
+    for (int i = 0; i < 2; ++i) {
+      this->v[i] += v[i];
+    }
+    return *this;
+  }
+  constexpr Vec2& operator-=(const Vec2& v) {
+    for (int i = 0; i < 2; ++i) {
+      this->v[i] -= v[i];
+    }
+    return *this;
+  }
+  constexpr Vec2& operator*=(const Vec2& v) {
+    for (int i = 0; i < 2; ++i) {
+      this->v[i] *= v[i];
+    }
+    return *this;
+  }
+  constexpr Vec2& operator/=(const Vec2& v) {
+    for (int i = 0; i < 2; ++i) {
+      this->v[i] /= v[i];
+    }
+    return *this;
+  }
+};
+
+inline constexpr Vec2 operator+(const Vec2& v1, const Vec2& v2) {
+  Vec2 ret;
+  for (int i = 0; i < 2; ++i) {
+    ret[i] = v1[i] + v2[i];
+  }
+  return ret;
+}
+inline constexpr Vec2 operator+(const Vec2& v1, float k) {
+  Vec2 ret;
+  for (int i = 0; i < 2; ++i) {
+    ret[i] = v1[i] + k;
+  }
+  return ret;
+}
+inline constexpr Vec2 operator+(float k, const Vec2& v2) {
+  Vec2 ret;
+  for (int i = 0; i < 2; ++i) {
+    ret[i] = k + v2[i];
+  }
+  return ret;
+}
+
+inline constexpr Vec2 operator-(const Vec2& v1, const Vec2& v2) {
+  Vec2 ret;
+  for (int i = 0; i < 2; ++i) {
+    ret[i] = v1[i] - v2[i];
+  }
+  return ret;
+}
+inline constexpr Vec2 operator-(const Vec2& v1, float k) {
+  Vec2 ret;
+  for (int i = 0; i < 2; ++i) {
+    ret[i] = v1[i] - k;
+  }
+  return ret;
+}
+inline constexpr Vec2 operator-(float k, const Vec2& v2) {
+  Vec2 ret;
+  for (int i = 0; i < 2; ++i) {
+    ret[i] = k - v2[i];
+  }
+  return ret;
+}
+
+inline constexpr Vec2 operator*(const Vec2& v1, const Vec2& v2) {
+  Vec2 ret;
+  for (int i = 0; i < 2; ++i) {
+    ret[i] = v1[i] * v2[i];
+  }
+  return ret;
+}
+inline constexpr Vec2 operator*(const Vec2& v1, float k) {
+  Vec2 ret;
+  for (int i = 0; i < 2; ++i) {
+    ret[i] = v1[i] * k;
+  }
+  return ret;
+}
+inline constexpr Vec2 operator*(float k, const Vec2& v2) {
+  Vec2 ret;
+  for (int i = 0; i < 2; ++i) {
+    ret[i] = k * v2[i];
+  }
+  return ret;
+}
+
+inline constexpr Vec2 operator/(const Vec2& v1, const Vec2& v2) {
+  Vec2 ret;
+  for (int i = 0; i < 2; ++i) {
+    ret[i] = v1[i] / v2[i];
+  }
+  return ret;
+}
+inline constexpr Vec2 operator/(const Vec2& v1, float k) {
+  Vec2 ret;
+  for (int i = 0; i < 2; ++i) {
+    ret[i] = v1[i] / k;
+  }
+  return ret;
+}
+inline constexpr Vec2 operator/(float k, const Vec2& v2) {
+  Vec2 ret;
+  for (int i = 0; i < 2; ++i) {
+    ret[i] = k / v2[i];
+  }
+  return ret;
+}
+
+inline constexpr float dot(const Vec2& v1, const Vec2& v2) {
+  float ret = 0;
+  for (int i = 0; i < 2; ++i) {
+    ret += v1[i] * v2[i];
+  }
+  return ret;
+}
+
+inline constexpr float length(const Vec2& v) { return std::sqrt(dot(v, v)); }
+inline constexpr float length2(const Vec2& v) { return dot(v, v); }
+
+}  // namespace LTRE
+
+#endif
