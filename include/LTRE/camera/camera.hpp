6fd788c8dd8997da182eabb60dde3e4d4f418f25
yumcyawiz <hashdolf1923@gmail.com>
2021-05-18 02:57:24 +0900

    add camera

---
+++ include/LTRE/camera/camera.hpp
@@ -0,0 +1,25 @@
+#ifndef _LTRE_CAMERA_H
+#define _LTRE_CAMERA_H
+#include "LTRE/core/ray.hpp"
+#include "LTRE/math/vec3.hpp"
+
+namespace LTRE {
+
+class Camera {
+ protected:
+  Vec3 camPos;
+  Vec3 camForward;
+  Vec3 camRight;
+  Vec3 camUp;
+
+  Camera(const Vec3& camPos, const Vec3& camForward) {
+    camRight = normalize(cross(camForward, Vec3(0, 1, 0)));
+    camUp = normalize(cross(camRight, camForward));
+  }
+
+  virtual bool sampleRay(float u, float v, Ray& ray, float& pdf) const = 0;
+};
+
+}  // namespace LTRE
+
+#endif
