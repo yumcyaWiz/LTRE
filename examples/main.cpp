ce7dc1111da0483b944191586c87a81ede907b30
yumcyawiz <hashdolf1923@gmail.com>
2021-05-17 17:35:30 +0900

    add intersect-info members

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,3 +1,4 @@
+#include "LTRE/core/intersect-info.hpp"
 #include "LTRE/core/ray.hpp"
 #include "LTRE/math/vec2.hpp"
 #include "LTRE/math/vec3.hpp"
--- include/LTRE/core/intersect-info.hpp
+++ include/LTRE/core/intersect-info.hpp
@@ -1,7 +1,8 @@
 #ifndef _INTERSECT_INFO_H
 #define _INTERSECT_INFO_H
 
-#include "LTRE/core/vec3.hpp"
+#include "LTRE/math/vec2.hpp"
+#include "LTRE/math/vec3.hpp"
 
 namespace LTRE {
 
@@ -9,6 +10,9 @@ struct IntersectInfo {
   float t;
   Vec3 hitPos;
   Vec3 hitNormal;
+  Vec2 uv;
+  unsigned int primID;
+  unsigned int geomID;
 };
 
 }  // namespace LTRE
