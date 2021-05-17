9a1b1da978e7be7bf91a858a318b604ecd6e2b51
yumcyawiz <hashdolf1923@gmail.com>
2021-05-18 02:34:08 +0900

    test film

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,4 +5,18 @@
 #include "LTRE/math/vec3.hpp"
 #include "LTRE/shape/sphere.hpp"
 
-int main() { return 0; }
+using namespace LTRE;
+
+int main() {
+  const int width = 512;
+  const int height = 512;
+
+  Film film(width, height);
+  for (int j = 0; j < height; ++j) {
+    for (int i = 0; i < width; ++i) {
+      film.setPixel(i, j, Vec3(i / 512.0f, j / 512.0f, 1.0f));
+    }
+  }
+
+  film.writePPM("output.ppm");
+}
