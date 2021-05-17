9e772442a25f6f5e36a14723ae5991a902c52126
yumcyawiz <hashdolf1923@gmail.com>
2021-05-17 17:03:21 +0900

    add vec3 test

---
+++ tests/vec3.cpp
@@ -0,0 +1,24 @@
+#include "LTRE/math/vec3.hpp"
+
+#include "gtest/gtest.h"
+
+using namespace LTRE;
+
+TEST(Vec3Operator, BasicAssertions) {
+  EXPECT_EQ(Vec3(0, 1, 2) + Vec3(3, 4, 5), Vec3(3, 5, 7));
+
+  EXPECT_EQ(Vec3(0, 1, 2) - Vec3(3, 4, 5), Vec3(-3, -3, -3));
+
+  EXPECT_EQ(Vec3(0, 1, 2) * 2, Vec3(0, 2, 4));
+  EXPECT_EQ(Vec3(0, 1, 2) * 2, Vec3(0, 2, 4));
+
+  EXPECT_EQ(Vec3(0, 2, 4) / 2, Vec3(0, 1, 2));
+  EXPECT_EQ(4 / Vec3(1, 2, 4), Vec3(4, 2, 1));
+
+  EXPECT_EQ(-Vec3(1, 2, 3), Vec3(-1, -2, -3));
+}
+
+TEST(Vec3Length, BasicAssertions) {
+  EXPECT_EQ(length(Vec3(1, 2, 2)), 3);
+  EXPECT_EQ(length2(Vec3(1, 2, 2)), 9);
+}
