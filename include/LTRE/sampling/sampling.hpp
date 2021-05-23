aa841df271e79983fe4d38bc8f18d4269d789198
yumcyawiz <hashdolf1923@gmail.com>
2021-05-23 23:26:04 +0900

    add sampling funcs

---
+++ include/LTRE/sampling/sampling.hpp
@@ -0,0 +1,12 @@
+#ifndef _LTRE_SAMPLING_H
+#define _LTRE_SAMPLING_H
+#include "LTRE/math/vec2.hpp"
+
+namespace LTRE {
+
+inline void sampleHemisphere(const Vec2& uv, float& pdf) { return; }
+inline void sampleCosineHemisphere(const Vec2& uv, float& pdf) { return; }
+
+}  // namespace LTRE
+
+#endif
