8ada348546a362752fe24992a1d6a037a6c939b0
yumcyawiz <hashdolf1923@gmail.com>
2021-05-28 00:19:11 +0900

    implement lambert bsdf

--- include/LTRE/bsdf/bsdf.hpp
+++ include/LTRE/bsdf/bsdf.hpp
@@ -2,13 +2,15 @@
 #define _LTRE_BSDF_H
 #include "LTRE/core/intersect-info.hpp"
 #include "LTRE/math/vec3.hpp"
+#include "LTRE/sampling/sampler.hpp"
 
 namespace LTRE {
 
 class BSDF {
  public:
-  virtual Vec3 bsdf(const Vec3& wi, const Vec3& wo) const = 0;
-  virtual Vec3 sample(const IntersectInfo& info, float& pdf) const = 0;
+  virtual Vec3 bsdf(const IntersectInfo& info, const Vec3& wi) const = 0;
+  virtual Vec3 sample(const IntersectInfo& info, Sampler& sampler, Vec3& wi,
+                      float& pdf) const = 0;
 };
 
 }  // namespace LTRE
--- include/LTRE/bsdf/lambert.hpp
+++ include/LTRE/bsdf/lambert.hpp
@@ -1,6 +1,7 @@
 #ifndef _LTRE_LAMBERT_H
 #define _LTRE_LAMBERT_H
 #include "LTRE/bsdf/bsdf.hpp"
+#include "LTRE/sampling/sampling.hpp"
 
 namespace LTRE {
 
@@ -9,6 +10,15 @@ class Lambert : public BSDF {
   const Vec3 rho;
 
   Lambert(const Vec3& rho) : rho(rho) {}
+
+  Vec3 bsdf(const IntersectInfo& info, const Vec3& wi) const override {
+    return rho / PI;
+  }
+  Vec3 sample(const IntersectInfo& info, Sampler& sampler, Vec3& wi,
+              float& pdf) const {
+    wi = sampleCosineHemisphere(sampler.getNext2D(), pdf);
+    return bsdf(info, wi);
+  }
 };
 
 }  // namespace LTRE
