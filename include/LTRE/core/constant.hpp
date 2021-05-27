9e77605c2df4c313e770981e561f82a3d4caf75d
yumcyawiz <hashdolf1923@gmail.com>
2021-05-27 22:23:01 +0900

    add constant

---
+++ include/LTRE/core/constant.hpp
@@ -0,0 +1,13 @@
+#ifndef _LTRE_CONSTANT_H
+#define _LTRE_CONSTANT_H
+
+namespace LTRE {
+
+constexpr float PI = 3.14159265359f;
+constexpr float PI_MUL_2 = 2.0f * PI;
+constexpr float PI_INV = 1.0f / PI;
+constexpr float PI_MUL_2_INV = 1.0f / PI_MUL_2;
+
+}  // namespace LTRE
+
+#endif
