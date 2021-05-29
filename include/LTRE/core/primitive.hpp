c5aab89dc9f4c0d9a37b69ffbc395c8d59c555b4
yumcyawiz <hashdolf1923@gmail.com>
2021-05-29 12:04:30 +0900

    implement bsdf sampling on primitive

--- include/LTRE/core/primitive.hpp
+++ include/LTRE/core/primitive.hpp
@@ -23,6 +23,25 @@ struct Primitive {
       return false;
     }
   }
+
+  Vec3 sampleBSDF(const Vec3& wo, const IntersectInfo& info, Sampler& sampler,
+                  Vec3& wi, float& pdf) const {
+    // compute tangent space basis
+    Vec3 t, b;
+    orthonormalBasis(info.hitNormal, t, b);
+
+    // world to local transform
+    const Vec3 wo_l = worldToLocal(wo, t, info.hitNormal, b);
+
+    // sample direction in tangent space
+    Vec3 wi_l;
+    const Vec3 bsdf = this->bsdf->sample(info, sampler, wi_l, pdf);
+
+    // local to world transform
+    wi = localToWorld(wi_l, t, info.hitNormal, b);
+
+    return bsdf;
+  }
 };
 
 }  // namespace LTRE
