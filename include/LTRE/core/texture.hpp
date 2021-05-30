5535eb12b4c5e1db97d6b027e00aa172eae5c711
yumcyawiz <hashdolf1923@gmail.com>
2021-05-30 10:55:15 +0900

    add image texture

--- include/LTRE/core/texture.hpp
+++ include/LTRE/core/texture.hpp
@@ -1,42 +1,64 @@
 #ifndef _LTRE_TEXTURE_H
 #define _LTRE_TEXTURE_H
+#include <filesystem>
 
-#include "LTRE/math/vec2.hpp"
+#include "spdlog/spdlog.h"
+#include "stb_image.h"
+//
+#include "LTRE/core/intersect-info.hpp"
 
 namespace LTRE {
 
 template <typename T>
 class Texture {
  public:
-  T getValue(const Vec2& uv) const = 0;
+  virtual ~Texture() {}
+  virtual T sample(const IntersectInfo& info) const = 0;
 };
 
 template <typename T>
-class UniformTexture {
+class UniformTexture : public Texture<T> {
  private:
   T value;
 
  public:
   UniformTexture(const T& value) : value(value) {}
 
-  T getValue(const Vec2& uv) const override { return value; };
+  T sample(const IntersectInfo& info) const override { return value; };
 };
 
-template <typename T>
-class ImageTexture {
+class ImageTexture : public Texture<Vec3> {
  private:
   int width;
   int height;
-  T* pixels;
+  unsigned char* image;
+
+  void loadImage(const std::filesystem::path& filepath) {
+    // load image with stb_image
+    int width, height, channels;
+    unsigned char* image = stbi_load(filepath.generic_string().c_str(), &width,
+                                     &height, &channels, 3);
+    if (!image) {
+      spdlog::error("failed to open " + filepath.string());
+      return;
+    }
+  }
 
  public:
-  ImageTexture(const std::string& filename) {}
-  ~ImageTexture() { delete[] pixels; }
+  ImageTexture(const std::filesystem::path& filepath) { loadImage(filepath); }
 
-  T getValue(const Vec2& uv) const override {
-    const int i = std::clamp(static_cast<int>(width * uv[0]), 0, width);
-    const int j = std::clamp(static_cast<int>(height * uv[1]), 0, height);
-    return pixels[i + width * j];
+  ~ImageTexture() override {
+    if (image) {
+      stbi_image_free(image);
+    }
+  }
+
+  Vec3 sample(const IntersectInfo& info) const override {
+    const int i = std::clamp(static_cast<int>(width * info.uv[0]), 0, width);
+    const int j = std::clamp(static_cast<int>(height * info.uv[1]), 0, height);
+    const int idx = 3 * i + 3 * width * j;
+    return Vec3(image[idx] / 255.0f, image[idx + 1] / 255.0f,
+                image[idx + 2] / 255.0f);
   }
 };
 
