f25f298cc30743a2d6a0851c25487be559bfc185
yumcyawiz <hashdolf1923@gmail.com>
2021-06-02 17:25:14 +0900

    test sponza

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,13 +1,15 @@
 #include "LTRE/bsdf/lambert.hpp"
 #include "LTRE/camera/pinhole-camera.hpp"
+#include "LTRE/core/model.hpp"
 #include "LTRE/core/renderer.hpp"
 #include "LTRE/core/scene.hpp"
 #include "LTRE/core/texture.hpp"
 #include "LTRE/integrator/pt.hpp"
 #include "LTRE/intersector/bvh.hpp"
+#include "LTRE/intersector/linear-intersector.hpp"
 #include "LTRE/sampling/uniform.hpp"
+#include "LTRE/shape/mesh.hpp"
 #include "LTRE/shape/sphere.hpp"
-#include "LTRE/shape/triangle.hpp"
 
 using namespace LTRE;
 
@@ -15,24 +17,27 @@ int main() {
   const int width = 512;
   const int height = 512;
 
-  const auto sphere1 = std::make_shared<Sphere>(Vec3(0, -1001, 0), 1000);
-  const auto sphere2 = std::make_shared<Sphere>(Vec3(0), 1);
-  const auto mat1 = std::make_shared<Lambert>(Vec3(0.8));
-  const auto prim1 = Primitive(sphere1, mat1);
-  const auto prim2 = Primitive(sphere2, mat1);
-
   const auto intersector = std::make_shared<BVH<Primitive>>();
   Scene scene(intersector);
-  scene.addPrimitive(prim1);
-  scene.addPrimitive(prim2);
+
+  // const auto sphere1 = std::make_shared<Sphere>(Vec3(0, -1001, 0), 1000);
+  // const auto sphere2 = std::make_shared<Sphere>(Vec3(0), 1);
+  // const auto mat1 = std::make_shared<Lambert>(Vec3(0.8));
+  // const auto prim1 = Primitive(sphere1, mat1);
+  // const auto prim2 = Primitive(sphere2, mat1);
+  // scene.addPrimitive(prim1);
+  // scene.addPrimitive(prim2);
+
+  const auto model = Model("../assets/sponza/sponza.obj");
+  scene.addModel(model);
   scene.build();
 
   const auto camera =
-      std::make_shared<PinholeCamera>(Vec3(0, 1, 5), Vec3(0, 0, -1));
+      std::make_shared<PinholeCamera>(Vec3(-10, 7, 0), Vec3(1, 0, 0));
   const auto integrator = std::make_shared<PT>();
   const auto sampler = std::make_shared<UniformSampler>();
 
   Renderer renderer(width, height, camera, integrator, sampler);
-  renderer.render(scene, 100);
+  renderer.render(scene, 10);
   renderer.writePPM("output.ppm");
 }
