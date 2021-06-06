7107a1326ee713b148d5f3dbe1e91560968089ae
yumcyawiz <hashdolf1923@gmail.com>
2021-06-07 04:50:42 +0900

    add print operator for intersect-info

--- include/LTRE/core/intersect-info.hpp
+++ include/LTRE/core/intersect-info.hpp
@@ -1,5 +1,6 @@
 #ifndef _INTERSECT_INFO_H
 #define _INTERSECT_INFO_H
+#include <iostream>
 
 #include "LTRE/math/vec2.hpp"
 #include "LTRE/math/vec3.hpp"
@@ -16,6 +17,16 @@ struct IntersectInfo {
   Vec2 barycentric;
   const Primitive* hitPrimitive;
 };
+
+inline std::ostream& operator<<(std::ostream& stream,
+                                const IntersectInfo& info) {
+  stream << "t: " << info.t << std::endl;
+  stream << "hitPos: " << info.hitPos << std::endl;
+  stream << "hitNormal: " << info.hitNormal << std::endl;
+  stream << "uv: " << info.uv << std::endl;
+  stream << "barycentric: " << info.barycentric << std::endl;
+  return stream;
+}
 
 }  // namespace LTRE
 
