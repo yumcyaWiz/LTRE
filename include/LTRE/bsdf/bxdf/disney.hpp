c138f527a6c778929a7ab6f2bc5a3bebbb0b55c9
yumcyawiz <hashdolf1923@gmail.com>
2021-06-11 01:36:35 +0900

    fix disney brdfs

--- include/LTRE/bsdf/bxdf/disney.hpp
+++ include/LTRE/bsdf/bxdf/disney.hpp
@@ -7,91 +7,107 @@
 namespace LTRE {
 
 inline float schlickFresnelR(const Vec3& w, float f0) {
-  return f0 +
-         (1.0f - f0) * std::pow(1.0f - std::max(BxDF::cosTheta(w), 0.0f), 5.0f);
+  constexpr auto pow5 = [](float x) { return x * x * x * x * x; };
+  return f0 + (1.0f - f0) * pow5(std::max(1.0f - BxDF::absCosTheta(w), 0.0f));
 }
 
 inline float schlickFresnelT(const Vec3& w, const Vec3& h, float f90) {
-  return 1.0f + (f90 - 1.0f) * std::pow(1.0f - std::max(dot(w, h), 0.0f), 5.0f);
+  constexpr auto pow5 = [](float x) { return x * x * x * x * x; };
+  return 1.0f + (f90 - 1.0f) * pow5(std::max(1.0f - std::abs(dot(w, h)), 0.0f));
 }
 
 class DisneyDiffuse : public BxDF {
  private:
-  Lambert lambert;
+  const Lambert lambert;
+  const float roughness;
 
  public:
-  DisneyDiffuse() {}
+  DisneyDiffuse(const Vec3& baseColor, float roughness)
+      : lambert{baseColor}, roughness{roughness} {}
 
-  Vec3 f(const BxDFArgs& args) const override {
-    const Vec3 h = normalize(args.wo + args.wi);
+  Vec3 f(const Vec3& wo, const Vec3& wi) const override {
+    // compute half-vector
+    Vec3 wh = wo + wi;
+    if (wh[0] == 0 && wh[1] == 0 && wh[2] == 0) return Vec3(0);
+    wh = normalize(wh);
 
-    const float cosD = dot(h, args.wi);
-    const float f90 = 0.5f + 2.0f * args.roughness * cosD * cosD;
+    const float cosD = std::abs(dot(wh, wi));
+    const float f90 = 0.5f + 2.0f * roughness * cosD * cosD;
 
-    return lambert.f(args) * schlickFresnelT(args.wi, h, f90) *
-           schlickFresnelT(args.wo, h, f90);
+    return lambert.f(wo, wi) * schlickFresnelT(wi, wh, f90) *
+           schlickFresnelT(wo, wh, f90);
   }
 
-  Vec3 sample(Sampler& sampler, BxDFArgs& args, float& pdf) const {
-    args.wi = sampleCosineHemisphere(sampler.getNext2D(), pdf);
-    return f(args);
+  Vec3 sample(Sampler& sampler, const Vec3& wo, Vec3& wi, float& pdf) const {
+    wi = sampleCosineHemisphere(sampler.getNext2D(), pdf);
+    return f(wo, wi);
   }
 };
 
 class DisneySubsurface : public BxDF {
  private:
-  Lambert lambert;
+  const Lambert lambert;
+  const float roughness;
 
  public:
-  DisneySubsurface() {}
+  DisneySubsurface(const Vec3& baseColor, float roughness)
+      : lambert{baseColor}, roughness(roughness) {}
 
-  Vec3 f(const BxDFArgs& args) const override {
-    const Vec3 h = normalize(args.wo + args.wi);
+  Vec3 f(const Vec3& wo, const Vec3& wi) const override {
+    const float cosThetaI = cosTheta(wi);
+    const float cosThetaO = cosTheta(wo);
+    if (cosThetaI == 0 || cosThetaO == 0) return Vec3(0);
 
-    const float cosD = dot(h, args.wi);
-    const float f90 = args.roughness * cosD * cosD;
-    const float cosThetaI = cosTheta(args.wi);
-    const float cosThetaO = cosTheta(args.wo);
+    // compute half-vector
+    Vec3 wh = wo + wi;
+    if (wh[0] == 0 && wh[1] == 0 && wh[2] == 0) return Vec3(0);
+    wh = normalize(wh);
 
-    constexpr float EPS = 1e-9f;
-    if (std::abs(cosThetaI * cosThetaO) < EPS) {
-      return Vec3(0);
-    }
+    const float cosD = std::abs(dot(wh, wi));
+    const float f90 = roughness * cosD * cosD;
 
-    return lambert.f(args) * 1.25f *
-           (schlickFresnelT(args.wi, h, f90) *
-                schlickFresnelT(args.wo, h, f90) *
+    return lambert.f(wo, wi) * 1.25f *
+           (schlickFresnelT(wi, wh, f90) * schlickFresnelT(wo, wh, f90) *
                 (1.0f / (cosThetaI * cosThetaO) - 0.5f) +
             0.5f);
   }
 
-  Vec3 sample(Sampler& sampler, BxDFArgs& args, float& pdf) const {
-    args.wi = sampleCosineHemisphere(sampler.getNext2D(), pdf);
-    return f(args);
+  Vec3 sample(Sampler& sampler, const Vec3& wo, Vec3& wi, float& pdf) const {
+    wi = sampleCosineHemisphere(sampler.getNext2D(), pdf);
+    return f(wo, wi);
   }
 };
 
 class DisneySheen : public BxDF {
  public:
-  DisneySheen() {}
+  const Vec3 baseColor;
+  const float sheen;
+  const float sheenTint;
+  Vec3 rho_tint;
+  Vec3 rho_sheen;
 
-  Vec3 f(const BxDFArgs& args) const override {
-    const Vec3 h = normalize(args.wi + args.wo);
-
-    const float luminance = RGBToXYZ(args.baseColor)[1];
-    Vec3 rho_tint;
+  DisneySheen(const Vec3& baseColor, float sheen, float sheenTint)
+      : baseColor{baseColor}, sheen(sheen), sheenTint(sheenTint) {
+    const float luminance = RGBToXYZ(baseColor)[1];
     if (luminance > 0) {
-      rho_tint = args.baseColor / luminance;
+      rho_tint = baseColor / luminance;
     }
-    const Vec3 rho_sheen = lerp(Vec3(1), rho_tint, args.sheenTint);
-
-    return args.sheen * rho_sheen *
-           std::pow(std::max(1.0f - dot(args.wi, h), 0.0f), 5.0f);
+    rho_sheen = lerp(Vec3(1), rho_tint, sheenTint);
   }
 
-  Vec3 sample(Sampler& sampler, BxDFArgs& args, float& pdf) const {
-    args.wi = sampleCosineHemisphere(sampler.getNext2D(), pdf);
-    return f(args);
+  Vec3 f(const Vec3& wo, const Vec3& wi) const override {
+    // compute half-vector
+    Vec3 wh = wo + wi;
+    if (wh[0] == 0 && wh[1] == 0 && wh[2] == 0) return Vec3(0);
+    wh = normalize(wh);
+
+    constexpr auto pow5 = [](float x) { return x * x * x * x * x; };
+    return sheen * rho_sheen * pow5(std::max(1.0f - dot(wi, wh), 0.0f));
+  }
+
+  Vec3 sample(Sampler& sampler, const Vec3& wo, Vec3& wi, float& pdf) const {
+    wi = sampleCosineHemisphere(sampler.getNext2D(), pdf);
+    return f(wo, wi);
   }
 };
 
