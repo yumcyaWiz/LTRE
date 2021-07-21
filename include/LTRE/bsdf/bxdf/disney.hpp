0695a3a67580bc15748a7a481bde4f8c3e9f9573
yumcyawiz <hashdolf1923@gmail.com>
2021-07-21 20:02:27 +0900

    match disney specular brdf to original

--- include/LTRE/bsdf/bxdf/disney.hpp
+++ include/LTRE/bsdf/bxdf/disney.hpp
@@ -132,9 +132,41 @@ class DisneySheen : public BxDF {
 
 class DisneySpecular : public BxDF {
  private:
-  MicrofacetBRDF microfacetBRDF;
+  float alpha;
   FresnelSchlick F;
-  GGX G;
+
+  // GGX D
+  float D(const Vec3& wh) const {
+    const float tan2Theta = BxDF::tan2Theta(wh);
+    if (std::isinf(tan2Theta)) return 0;
+    const float cos4Theta = BxDF::cos2Theta(wh) * BxDF::cos2Theta(wh);
+
+    const float term = 1.0f + tan2Theta / (alpha * alpha);
+    return 1.0f / ((PI * alpha * alpha * cos4Theta) * term * term);
+  }
+
+  Vec3 sample(const Vec2& uv, float& pdf) const {
+    const float theta = std::atan((alpha * std::sqrt(std::max(uv[0], 0.0f))) /
+                                  std::sqrt(std::max(1.0f - uv[0], 0.0f)));
+    const float phi = 2.0f * PI * uv[1];
+    const Vec3 wh = sphericalToCartesian(theta, phi);
+    pdf = D(wh) * BxDF::absCosTheta(wh);
+    return wh;
+  }
+
+  // GGX Lambda
+  float Lambda(const Vec3& w) const {
+    const float absTanTheta = BxDF::absTanTheta(w);
+    if (std::isinf(absTanTheta)) return 0;
+
+    const float alpha2Tan2Theta = (alpha * absTanTheta) * (alpha * absTanTheta);
+    return 0.5f * (-1.0f + std::sqrt(1.0f + alpha2Tan2Theta));
+  }
+
+  float G1(const Vec3& w) const { return 1.0f / (1.0f + Lambda(w)); }
+
+  // separable masking-shadowing
+  float G2(const Vec3& wo, const Vec3& wi) const { return G1(wo) * G1(wi); }
 
  public:
   DisneySpecular(const Vec3& baseColor, float roughness, float specular,
@@ -155,21 +187,51 @@ class DisneySpecular : public BxDF {
     // aspect); const float alphaY = std::max(0.001f, roughness * roughness *
     // aspect);
     F = FresnelSchlick(f0);
-    G = GGX(std::max(roughness * roughness, 0.001f));
-    microfacetBRDF = MicrofacetBRDF(&F, &G);
+    alpha = std::max(roughness * roughness, 0.001f);
   }
 
   Vec3 f(const Vec3& wo, const Vec3& wi) const override {
-    return microfacetBRDF.f(wo, wi);
+    const float cosThetaO = absCosTheta(wo);
+    const float cosThetaI = absCosTheta(wi);
+    if (cosThetaI == 0 || cosThetaO == 0) return Vec3(0);
+
+    // compute half-vector
+    Vec3 wh = wo + wi;
+    if (wh[0] == 0 && wh[1] == 0 && wh[2] == 0) return Vec3(0);
+    wh = normalize(wh);
+
+    const Vec3 f = F.evaluate(dot(wo, wh));
+    const float d = D(wh);
+    const float g = G2(wo, wi);
+    return f * d * g / (4.0f * cosThetaO * cosThetaI);
   }
 
   Vec3 sample(Sampler& sampler, const Vec3& wo, Vec3& wi,
               float& pdf) const override {
-    return microfacetBRDF.sample(sampler, wo, wi, pdf);
+    // sample half-vector
+    float pdf_wh;
+    const Vec3 wh = sample(sampler.getNext2D(), pdf_wh);
+
+    // compute indident direction
+    wi = BxDF::reflect(wo, wh);
+
+    // convert hald-vector pdf to incident direction pdf
+    pdf = pdf_wh / (4.0f * dot(wi, wh));
+
+    return f(wo, wi);
   }
 
   float pdf(const Vec3& wo, const Vec3& wi) const override {
-    return microfacetBRDF.pdf(wo, wi);
+    // compute half-vector
+    Vec3 wh = wo + wi;
+    if (wh[0] == 0 && wh[1] == 0 && wh[2] == 0) return 0;
+    wh = normalize(wh);
+
+    // compute half-vector pdf
+    const float pdf_wh = D(wh) * absCosTheta(wh);
+
+    // convert hald-vector pdf to incident direction pdf
+    return pdf_wh / (4.0f * dot(wi, wh));
   }
 };
 
